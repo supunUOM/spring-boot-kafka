a89d6758d997ee3ccc9bd77a98e1a8a830c5c723
supunuom <adsupun97@gmail.com>
2023-10-11 09:04:08 +0530

    only json serialization part

--- src/main/java/net/javaguides/springboot/config/KafkaTopicConfig.java
+++ src/main/java/net/javaguides/springboot/config/KafkaTopicConfig.java
@@ -9,17 +9,8 @@ import org.springframework.kafka.config.TopicBuilder;
 @Configuration
 public class KafkaTopicConfig {
 
-    @Value("${spring.kafka.topic.name}")
-    private String topicName;
-
     @Value("${spring.kafka.topic-json.name}")
     private String topicJsonName;
-
-    @Bean
-    public NewTopic javaguidesTopic(){
-        return TopicBuilder.name(topicName)
-                .build();
-    }
 
     @Bean
     public NewTopic javaguidesJsonTopic(){
