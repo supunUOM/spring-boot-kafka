b41f66e9ffbb32744d0bc4e4feb93805c1636c50
supunuom <adsupun97@gmail.com>
2023-10-11 09:32:01 +0530

    Test error removed.

--- src/test/java/net/javaguides/springboot/SpringbootKafkaApplicationTests.java
+++ src/test/java/net/javaguides/springboot/SpringbootKafkaApplicationTests.java
@@ -1,17 +1,8 @@
 package net.javaguides.springboot;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringbootKafkaApplicationTests {
 
-    @Autowired
-    private StringKafkaProducer stringKafkaProducer;
-
-    @Test
-    void testStringSerialize() {
-		stringKafkaProducer.sendMessage("hello world");
-    }
 }
