42736b1a04ce45172f70017993ed9fe526f2d78d
supunuom <adsupun97@gmail.com>
2023-10-11 09:28:51 +0530

    only json-serialization part.

--- src/test/java/net/javaguides/springboot/SpringbootKafkaApplicationTests.java
+++ src/test/java/net/javaguides/springboot/SpringbootKafkaApplicationTests.java
@@ -1,6 +1,5 @@
 package net.javaguides.springboot;
 
-import net.javaguides.springboot.kafka.StringKafkaProducer;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
